86fa4447862d58aceb05d0273c6c80e423786f5c
Ridwan_AI <88805381+RidwendDev@users.noreply.github.com>
2022-08-03 10:10:37 +0700

    Update halo.py
    mengoreksi sdikit ksalahan

--- halo.py
+++ halo.py
@@ -1,5 +1,5 @@
 print('halo nama kamu siapa?')
 # oke itu kita cuma minta inputan tapi kenapa, tidak langsung disapa dengan menggunakan format
-nama = input(nama)
-lokasi = input(lokasi)
+nama = input()
+lokasi = input()
 print(f'halo nama kamu siapa?, oiya kenalin namaku {nama} dari {lokasi}. Salken ya')
